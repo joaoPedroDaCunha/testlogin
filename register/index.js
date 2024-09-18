767393f25af33b85358bdcd4c352e8fad665dd1d
unknown <joaopedrodacunha.santos@gmail.com>
2024-09-18 10:20:05 -0300

    register js ajustes

---
+++ register/index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded',() =>{
+    //entrada de dados
+    const email = document.getElementById('email');
+    const senha = document.getElementById('password1');
+    const senha2 = document.getElementById('password2');
+    const nome = document.getElementById('primeiroNome');
+    const sobrenome =document.getElementById('segundoNome');
+
+    //botão de eventos
+    const bota = document.getElementById('_reg');
+
+    bota.addEventListener('click',() =>{
+       
+    })
+
+});
