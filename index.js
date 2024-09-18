8504168cc8b24add279887fb2cf96034f588c1c8
unknown <joaopedrodacunha.santos@gmail.com>
2024-09-18 08:43:28 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const login = document.getElementById("log");
     const register = document.getElementById("regis");
-    const n = document.getElementById("_n");
-    const p = document.getElementById("_p");
+    const n = document.getElementById("email");
+    const p = document.getElementById("password");
 
-    login.addEventListener("click", function() {
+    login.addEventListener("click", () => {
         n.value;
         p.value;
         //para obter o que esta escrito dentro da função utiliza o value se não ira obter so as marcações do html 
          
     });
 
-    register.addEventListener("click", function() {
+    register.addEventListener("click", () => {
         window.location.href = "./register";
         //comando para redirecionar para outro diretorio
     });
